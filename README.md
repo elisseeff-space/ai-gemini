97ecb1b976735fbcc152018d59c50a343d55fce5
pavel <pavel@gmail.com>
2024-02-05 21:59:09 +0300

    1

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ai-gemini
 https://ai.google.dev/api/python/google/generativeai
+https://ai.google.dev/tutorials/python_quickstart#chat_conversations
+
 
 
 pip install google-generativeai
--- chat_conversations.py
+++ chat_conversations.py
@@ -8,8 +8,12 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
+response = None
+#response = chat.send_message("Okay, how about a more detailed explanation to a high schooler?", stream=True)
+response = chat.send_message("Грэмми - это, конечно, хрень какая-то. Очевидно же, что лучший рок-альбом прошлого года - это Life is But a Dream... группы Avenged Sevenfold. А они их даже номинировать забыли.", stream=True, safety_settings={'HARASSMENT':'block_none'})
 
-response = chat.send_message("Okay, how about a more detailed explanation to a high schooler?", stream=True)
+print(response.prompt_feedback)
+input()
 
 for chunk in response:
   print(chunk.text)
