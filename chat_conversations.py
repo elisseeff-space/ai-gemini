dd945d9f29f036d5296b65093cd2f64e0bd9b2a8
pavel <pavel@gmail.com>
2024-02-06 21:19:49 +0300

    3

--- chat_conversations.py
+++ chat_conversations.py
@@ -10,14 +10,22 @@ model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
 response = None
 #response = chat.send_message("Okay, how about a more detailed explanation to a high schooler?", stream=True)
-response = chat.send_message("Грэмми - это, конечно, хрень какая-то. Очевидно же, что лучший рок-альбом прошлого года - это Life is But a Dream... группы Avenged Sevenfold. А они их даже номинировать забыли.", stream=True, safety_settings={'HARASSMENT':'block_none'})
+response = chat.send_message("Грэмми - это, конечно, хрень какая-то. Очевидно же, что лучший рок-альбом прошлого года - это Life is But a Dream... группы Avenged Sevenfold. А они их даже номинировать забыли.", safety_settings={'HARASSMENT':'block_none'})#, stream=True, safety_settings={'HARASSMENT':'block_none'})
 
-print(response.prompt_feedback)
-input()
+for _ in response.prompt_feedback.safety_ratings :
+  print(_)
+  print('category: ', _.category)
+  print('probability: ', _.probability)
+  #input()
 
-for chunk in response:
-  print(chunk.text)
-  print("_"*80)
+try:
+  print(response.text)
+except Exception as e:
+  print(f'{type(e).__name__}: {e}')
+
+#for chunk in response:
+#  print(chunk.text)
+#  print("_"*80)
 
 
 #print(chat.history)
